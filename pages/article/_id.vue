8fff6e2a2039985b77a5338d8e4a7753d836bc70
Stanislav <116022586+okstas86@users.noreply.github.com>
2023-08-27 17:16:21 +0200

    Update _id.vue

--- pages/article/_id.vue
+++ pages/article/_id.vue
@@ -25,18 +25,26 @@ export default {
   width: 97%;
   background-color: #353030;
   color: #fafafa;
-  text-align: center;
-  padding: 20px;
+  text-align: center; 
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
 }
 h1 {
-  margin-bottom: 4rem;
+  margin-bottom: 2rem;
 }
 
 div p {
   font-size: 20px;
   text-align: center;
+}
+@media (max-width: 600px) {
+  h1 {
+
+    font-size: 22px; /
+  }
+
+  div p {
+    font-size: 16px;
+  }
 }
 </style>
